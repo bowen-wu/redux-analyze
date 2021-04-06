3f1b3bad2b6574c5980fa815647d8f70e766dd63
wubowen <wubowen@aldt.com.cn>
2021-04-06 17:12:12 +0800

    feat(selfAchieve): 更新 store 组件可以响应 	目前问题：connect 组件之后组件没有使用 state，state 更新后组件 render

--- src/features/selfAchieve/redux.js
+++ src/features/selfAchieve/redux.js
@@ -1,20 +1,43 @@
-import React, {useContext} from 'react';
-import reducers from './reducers';
+import React, {useContext, useEffect, useState} from 'react';
 
 export const store = {
   state: {user: {name: 'react', age: 18}},
   setState(newState) {
     store.state = newState;
+    store.listener.map(callback => callback());
+  },
+  listener: [],
+  subscribe(callback) {
+    store.listener.push(callback);
   }
 }
+
+export const reducers = (state, action) => {
+  switch (action.type) {
+    case 'updateUserName':
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload
+        }
+      };
+    default:
+      throw new Error('unknown Action Type');
+  }
+};
 
 export const appContext = React.createContext(null);
 
 export const connect = component => props => {
   const appContextValue = useContext(appContext);
+  const [, setUpdate] = useState({});
+
+  useEffect(() => {
+    appContextValue.subscribe(() => setUpdate({}));
+  }, [appContextValue]);
 
   const updateState = (action) => {
-    console.log(action);
     appContextValue.setState(reducers(appContextValue.state, action));
   };
 
